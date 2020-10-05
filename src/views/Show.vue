50934a8c84b68b624f2bd6a8153863a60088a6fa
koduki <pascalm3+git@gmail.com>
2020-10-04 22:02:57 -0700

    Bug fix

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -18,9 +18,7 @@
       <div class="container">
         <b-tabs content-class="mt-3">
           <b-tab title="Slide" active>
-            <div v-if="isLoading==false">
-              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Now Loading
-            </div>
+
             <b-carousel
               id="slideShow"
               v-model="slide"
@@ -71,7 +69,6 @@ export default {
   created: function () {
     this.isLoading = true;
     this.loadSlide(this.$route.params.userid, this.$route.params.key);
-    this.isLoading = false;
   },
   methods: {
     onCancel: function () {
@@ -80,7 +77,6 @@ export default {
     onTabVcas: function(){
       this.isLoading = true;
       this.loadSlide4Vcas(this.$route.params.userid, this.$route.params.key);
-      this.isLoading = false;
     },
     onTweet: function(){
         var shareURL = 'https://twitter.com/intent/tweet?text=' + "Slide4VR: " + this.slideItem.title + "+%23slide4vr" + '&url=' + location.href;
@@ -88,21 +84,21 @@ export default {
         window.open(shareURL, 'twitter', option)
     },
     loadSlide: function (id, key) {
-      this.isLoading = false;
       const uri = process.env.VUE_APP_API_BASE_URL + "/slide/"+ id + "/" + key + "/";
       this.axios.get(uri).then((response) => {
         this.slideItem.title = response.data.title;
         this.slideItem.created_at = response.data.created_at;
         this.slideItem.items = response.data.slides;
+        this.isLoading = false;
       });
     },
     loadSlide4Vcas: function (id, key) {
-      this.isLoading = false;
       const uri = process.env.VUE_APP_API_BASE_URL + "/slide/"+ id + "/" + key + "/?format=vcas";
       this.axios.get(uri).then((response) => {
         this.jsonTemplate = JSON.stringify(response.data, null, "  ")
           .slice(1, -2)
           .trim();
+        this.isLoading = false;
       });
     },
   }
